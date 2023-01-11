da55a4bc54f6aba9d090e22c3129518d6a1dd07d
Blanche Haddad <blancherachel@gmail.com>
2023-01-10 17:39:14 -0700

    feat: Add tests for Traveler Repo test file, all pass

---
+++ test/Traveler-Repository-test.js
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import TravelerRepository from "../src/Traveler-Repository";
+import travelers from "../src/data/travelers";
+
+describe("Traveler Repository", () => {
+  let travelerRepository;
+  let travelerData;
+  beforeEach(() => {
+    travelerData = travelers;
+    travelerRepository = new TravelerRepository(travelerData);
+  });
+  it("should be a function", () => {
+    expect(TravelerRepository).to.be.a("function");
+  });
+  it("should be an instance of TravelerRepository", () => {
+    expect(travelerRepository).to.be.an.instanceOf(TravelerRepository);
+  });
+  it("should hold all traveler data", () => {
+    expect(travelerRepository.allTravelerData).to.deep.equal(travelers);
+  });
+  it("should find the traveler by id", () => {
+    const a = {
+      id: 1,
+      name: "Ham Leadbeater",
+      travelerType: "relaxer",
+    };
+    expect(travelerRepository.findTravelerById(1)).to.deep.equal(a);
+  });
+});
