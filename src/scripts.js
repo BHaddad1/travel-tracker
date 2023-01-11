dfb8ad2af5b66899e8f056939618b627209d4482
Blanche Haddad <blancherachel@gmail.com>
2023-01-11 08:39:34 -0700

    fix: Update import for dayJS

--- src/scripts.js
+++ src/scripts.js
@@ -3,10 +3,9 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import * as dayjs from "dayjs";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
 import "./images/catTravel2.jpg"
 
-const dayjs = require('dayjs')
-dayjs().format()
