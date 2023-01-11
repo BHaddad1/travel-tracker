89227f5b40b2c7ed867976f565c3e9a472891498
Blanche Haddad <blancherachel@gmail.com>
2023-01-11 08:20:00 -0700

    feat: Add findTripsByDate(id) method

--- src/Trip-Repository.js
+++ src/Trip-Repository.js
@@ -1,13 +1,22 @@
+import * as dayjs from 'dayjs'
+
 class TripRepository {
   constructor(allTripData) {
     this.allTripData = allTripData;
   }
   filterByTravelerID(id) {
-    return this.allTripData.filter(trip => trip.userID === id);
+    return this.allTripData.filter((trip) => trip.userID === id);
   }
   filterTripsByStatus(status, id) {
-    return this.filterByTravelerID(id).filter(trip => trip.status === status);
+    return this.filterByTravelerID(id).filter((trip) => trip.status === status);
   }
-};
+  findTripsByDate(id) {
+    const today = dayjs("2021/12/31");
+    return this.filterByTravelerID(id).filter((trip) => {
+      let dateA = dayjs(trip.date);
+      return dateA.isAfter(today);
+    });
+  }
+}
 
 export default TripRepository;
