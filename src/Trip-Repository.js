fc5e34ae75472f9b38b58a91fe33f83f0907c2d9
Blanche Haddad <blancherachel@gmail.com>
2023-01-11 08:39:58 -0700

    feat: Add method to find past trips for traveler

--- src/Trip-Repository.js
+++ src/Trip-Repository.js
@@ -1,22 +1,29 @@
-import * as dayjs from 'dayjs'
+import * as dayjs from "dayjs";
 
 class TripRepository {
   constructor(allTripData) {
     this.allTripData = allTripData;
   }
   filterByTravelerID(id) {
-    return this.allTripData.filter((trip) => trip.userID === id);
+    return this.allTripData.filter(trip => trip.userID === id);
   }
   filterTripsByStatus(status, id) {
-    return this.filterByTravelerID(id).filter((trip) => trip.status === status);
+    return this.filterByTravelerID(id).filter(trip => trip.status === status);
   }
-  findTripsByDate(id) {
+  findUpcomingTrips(id) {
     const today = dayjs("2021/12/31");
-    return this.filterByTravelerID(id).filter((trip) => {
+    return this.filterByTravelerID(id).filter(trip => {
       let dateA = dayjs(trip.date);
       return dateA.isAfter(today);
     });
   }
-}
+  findPastTrips(id) {
+    const today = dayjs("2021/12/31");
+    return this.filterByTravelerID(id).filter(trip => {
+      let dateA = dayjs(trip.date);
+      return dateA.isBefore(today);
+    });
+  }
+};
 
 export default TripRepository;
