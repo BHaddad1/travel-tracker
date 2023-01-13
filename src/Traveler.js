45f5b712033da1f8619da6345fe50706e321c366
Blanche Haddad <blancherachel@gmail.com>
2023-01-13 09:00:52 -0700

    fix: Remove unused classes

--- src/Traveler.js
+++
@@ -1,9 +0,0 @@
-class Traveler {
-  constructor(travelerObj) {
-    this.id = travelerObj.id;
-    this.name = travelerObj.name;
-    this.travelerType = travelerObj.travelerType;
-  }
-};
-
-export default Traveler;
